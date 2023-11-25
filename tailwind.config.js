b81827dd1fda4674c5de66931548c3878b8ff37d
DevVali <90271108+DevVali@users.noreply.github.com>
2023-11-25 22:07:00 +0100

    feat: add tailwindcss example

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["resources/**/*.{juice,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
